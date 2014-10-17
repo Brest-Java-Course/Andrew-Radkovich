74f2bf1deb3cd610c34b591ead4e7cd560952551
Andrey <andrey.radkovich.work@mail.ru>
2014-10-17 16:22:30 +0300

    added user.java

--- app-core/src/main/java/com/epam/brest/courses/domain/User.java
+++ app-core/src/main/java/com/epam/brest/courses/domain/User.java
@@ -1,6 +1,32 @@
 package com.epam.brest.courses.domain;
 
-public class User
-{
-	
+public class User{
+	private long userId;
+	private String login;
+	private String userName;
+
+	public long getUserId()
+	{
+		return userId;
+	}
+	public String getLogin()
+	{
+		return login;
+	}
+	public String getUserName()
+	{
+		return userName;
+	}
+	public void setUserId(long userId)
+	{
+		this.userId=userId;
+	}
+	public void setLogin(String login)
+	{
+		this.login=login;
+	}
+	public void setUserId(String userName)
+	{
+		this.userName=userName;
+	}
 }
