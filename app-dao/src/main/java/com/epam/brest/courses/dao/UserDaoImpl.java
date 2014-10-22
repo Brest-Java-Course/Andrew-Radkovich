fdbe3f69b66840d1504a2bbde2bfa8ec58dfc82b
Andrey <andrey.radkovich.work@mail.ru>
2014-10-23 01:54:04 +0300

    no name commit

--- app-dao/src/main/java/com/epam/brest/courses/dao/UserDaoImpl.java
+++ app-dao/src/main/java/com/epam/brest/courses/dao/UserDaoImpl.java
@@ -1,16 +1,39 @@
 package com.epam.brest.courses.dao;
 
 import com.epam.brest.courses.domain.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 /**
  * Created by andrew on 20.10.14.
  */
 public class UserDaoImpl implements UserDao {
+    private JdbcTemplate jdbcTemplate;
+
+    public class UserMapper implements RowMapper<User> {
+
+        public User mapRow(ResultSet rs, int i) throws SQLException {
+            User user = new User();
+            user.setUserId(rs.getLong("userId"));
+            user.setLogin(rs.getString("login"));
+            user.setUserName(rs.getString("name"));
+            return user;
+        }
+    }
+
+    public void setDataSource(DataSource dataSource){
+        jdbcTemplate = new JdbcTemplate((javax.sql.DataSource) dataSource);
+    }
+
     @Override
-    public List<User> getUsers() {
-        return null;
+    public List<User> getUsers(){
+        //return null;
+        return jdbcTemplate.query("select userId, login, name from USER", new UserMapper());
     }
 
     @Override
@@ -20,6 +43,6 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public void addUser(User user) {
-
+        jdbcTemplate.update("INSERT INTO USER  (userId, login, name) VALUES(?,?,?)", user.getUserId(), user.getLogin(), user.getUserName());
     }
 }
--- app-dao/src/test/java/com/epam/brest/courses/dao/UserDaoImplTest.java
+++ app-dao/src/test/java/com/epam/brest/courses/dao/UserDaoImplTest.java
@@ -1,15 +1,44 @@
 package com.epam.brest.courses.dao;
 
 import com.epam.brest.courses.dao.UserDao;
+import com.epam.brest.courses.domain.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.swing.*;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by andrew on 22.10.14.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/home/andrew/SampleUserManagment/app-dao/src/test/resourses/testApplicationContextSpring.xml")
 public class UserDaoImplTest {
 
     @Autowired
     private UserDao userDao;
 
+    @Test
+    public void getUsers(){
+        List<User> userDaoList = userDao.getUsers();
+        assertNotNull(userDaoList);
+    }
+    public void addUser(){
+        List<User> users = userDao.getUsers();
+        int sizeBefore = users.size();
+        User user = new User();
+        user.setUserId(3);
+        user.setUserName("name");
+        user.setLogin("login");
+        userDao.addUser(user);
 
+        users = userDao.getUsers();
+
+        assertEquals(sizeBefore, users.size() - 1);
+    }
 }
