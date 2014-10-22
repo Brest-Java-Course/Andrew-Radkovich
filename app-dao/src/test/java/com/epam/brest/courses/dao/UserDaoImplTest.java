2748fa2118cac9c76303bd1fc39e4dced5f66f75
Andrey <andrey.radkovich.work@mail.ru>
2014-10-22 19:06:14 +0300

    added userDaoImplTes.java, testApplicationContextSpring.xml, modifided pom.xml & app-dao/pom.xml

---
+++ app-dao/src/test/java/com/epam/brest/courses/dao/UserDaoImplTest.java
@@ -0,0 +1,15 @@
+package com.epam.brest.courses.dao;
+
+import com.epam.brest.courses.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by andrew on 22.10.14.
+ */
+public class UserDaoImplTest {
+
+    @Autowired
+    private UserDao userDao;
+
+
+}
